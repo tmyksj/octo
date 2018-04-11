27fdfb625eb905aa7ea6026ea30528cf81cdf8e1
tmyksj <33417830+tmyksj@users.noreply.github.com>
2018-04-11 11:23:01 +0900

    add dialog component

--- octo.js
+++ octo.js
@@ -17,9 +17,27 @@
         }
     }
 
+    class OctoComponent {
+        constructor(root) {
+            this.root = root;
+        }
+
+        querySelector(selector) {
+            let elementArray = this.querySelectorAll(selector);
+            return (elementArray.length > 0 ? elementArray[0] : null);
+        }
+
+        querySelectorAll(selector) {
+            return Array.from(this.root.querySelectorAll(selector))
+                .concat(Array.from(document.querySelectorAll("[data-join]")).filter((value, index, array) => {
+                    return this.root.matches(value.dataset.join);
+                }));
+        }
+    }
+
     class OctoContext {
         constructor(selectorArray) {
-            this.def_ = {};
+            this.data_ = {};
             this.selectorArray_ = selectorArray;
         }
 
@@ -27,12 +45,12 @@
             return new OctoContextAssert(actual);
         }
 
-        def(key, value) {
+        data(key, value) {
             if (value !== undefined) {
-                this.def_[key] = value;
+                this.data_[key] = value;
             }
 
-            return this.def_[key];
+            return this.data_[key];
         }
 
         when() {
@@ -74,7 +92,7 @@
             return new OctoContextWhenDo((callback) => {
                 window.addEventListener("DOMContentLoaded", (event) => {
                     document.querySelectorAll(this.selectorArray_.join()).forEach((value) => {
-                        callback(value);
+                        callback(new OctoComponent(value), event);
                     });
                 });
             });
@@ -87,11 +105,22 @@
                         this.eventListenerObject_[value] = [];
                         document.addEventListener(value, (event) => {
                             this.eventListenerObject_[value].forEach((listener) => {
-                                for (let t = event.target; t !== null; t = t.parentElement) {
-                                    if (t.matches(this.selectorArray_.join())) {
-                                        listener(t);
+                                let propagation = (target) => {
+                                    if (target === null) {
+                                        return;
                                     }
-                                }
+
+                                    if (target.dataset.join === undefined) {
+                                        if (target.matches(this.selectorArray_.join())) {
+                                            listener(new OctoComponent(target), event);
+                                        }
+                                        propagation(target.parentElement);
+                                    } else {
+                                        document.querySelectorAll(target.dataset.join).forEach(propagation);
+                                    }
+                                };
+
+                                propagation(event.target);
                             });
                         });
                     }
@@ -112,10 +141,23 @@
         }
     }
 
+    Octo.target([".dialog"]).build((context) => {
+        context.when().event(["click"]).do((component, event) => {
+            let dialog = event.target.dataset.dialog;
+            context.assert(dialog).isNotNull();
+
+            if (dialog === "open") {
+                component.root.classList.add("dialog--open");
+            } else {
+                component.root.classList.remove("dialog--open");
+            }
+        });
+    });
+
     Octo.target([".field"]).build((context) => {
-        context.def("update", (target) => {
-            let control = target.querySelector(".field__control");
-            let label = target.querySelector(".field__label");
+        context.data("update", (component, event) => {
+            let control = component.querySelector(".field__control");
+            let label = component.querySelector(".field__label");
 
             context.assert(control).isNotNull();
             context.assert(label).isNotNull();
@@ -127,7 +169,7 @@
             }
         });
 
-        context.when().domContentLoaded().do(context.def("update"));
-        context.when().event(["change", "input"]).do(context.def("update"));
+        context.when().domContentLoaded().do(context.data("update"));
+        context.when().event(["change", "input"]).do(context.data("update"));
     });
 })();
